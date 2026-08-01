92f835fd224e7c8348c5b9437e15b9015dcf86eb
Krisha Nonghanvadra <krisha1667@gmail.com>
2026-08-01 14:57:39 +0530

    Implement JWT authentication service and improve user schemas

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.security import hash_password
 from app.models.user import User
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserResponse
+
+from fastapi import HTTPException
 
 
 router = APIRouter(
@@ -13,25 +15,36 @@ router = APIRouter(
 )
 
 
-@router.post("/register")
+@router.post(
+        "/register",
+        response_model = UserResponse,
+        status_code = 201
+)
 def register_user(
     user_data: UserCreate,
     db: Session = Depends(get_db)
 ):
+    existing_user = db.query(User).filter(
+        User.email == user_data.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400, 
+            detail="Email already registered."
+            )
+
     hashed_password = hash_password(user_data.password)
 
     new_user = User(
         email=user_data.email,
         password_hash=hashed_password
     )
+    
+  
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    return {
-        "message": "User registered successfully",
-        "user_id": new_user.id,
-        "email": new_user.email,
-        "role": new_user.role
-    }
+    return new_user
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,6 +1,29 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserCreate(BaseModel):
     email: EmailStr
+
+    password: str = Field(
+        min_length=8,
+        max_length=100,
+        description="Password must contain at least 8 characters."
+    )
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    role: str
+
+    class Config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
